07be36875731e14680bc7742a51795af8cd7e62c
ver-sim <verdianasimonetti@gmail.com>
2024-02-08 23:43:43 +0100

    simple set up express app

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,40 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import "express-async-errors"
 
-console.log(TODO);
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+type Planet = {
+    id: number,
+    name: string,
+  };
+
+  type Planets = Planet[];
+
+  let planets: Planets = [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+  ];
+
+app.use(express.json());
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.json(planets);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
+
