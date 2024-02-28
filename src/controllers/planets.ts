117fcdbc5d8c63d88407b935f4fef236ff9d9282
ver-sim <verdianasimonetti@gmail.com>
2024-02-28 14:52:05 +0100

    passport

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,26 +1,6 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import { loadavg } from "os";
-import pgPromise from "pg-promise";
-
-const db = pgPromise()("postgres://postgres:postgres@localhost:5432/planet");
-
-const setupDB = async () => {
-  await db.none(`
-    DROP TABLE IF EXISTS planets;
-
-    CREATE TABLE planets (
-      id SERIAL NOT NULL PRIMARY KEY,
-      name TEXT NOT NULL,
-      image TEXT
-    );
-  `)
-
-  await db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
-  await db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
-}
-
-setupDB();
+import { db } from "./../db.js"
 
 const getAll = async (req: Request, res: Response) => {
   const planets = await db.many(`SELECT * FROM planets;`)  
@@ -72,9 +52,6 @@ const deleteById = async (req: Request, res: Response) => {
 const addImg = async (req: Request, res: Response) => {
   const { id } = req.params;
   const fileName = req.file?.path;
-  // console.log(id)
-  // console.log(fileName)
-  console.log(req.file);
 
   if (fileName) {
     db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, fileName]);
---
+++ src/passport.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import passport from "passport";
+import passportJWT from "passport-jwt";
+import { db } from "./db";
+
+const SECRET_KEY = process.env.SECRET || 'secret';
+
+passport.use(
+    new passportJWT.Strategy({
+        secretOrKey: SECRET_KEY,
+        jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    }, 
+    async (payload, done) => {
+        const user = db.one(`SELECT * FROM users WHERE id=$1`, payload.id)
+
+        try {
+            return user ? done(null, user) : done(new Error("user not found"));
+        } catch (error) {
+            done(error)
+        }
+    }
+    )
+);
